6c2825449206450fbc4da67a373ba5600fc186d6
ismael <ismael.montealegre@gmail.com>
2018-07-19 19:45:36 +0200

    Add Sleuth tracing

--- src/main/java/img/controllers/SearchController.java
+++ src/main/java/img/controllers/SearchController.java
@@ -2,9 +2,10 @@ package img.controllers;
 
 import img.model.api.Document;
 import img.service.SearchService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,11 +13,15 @@ import java.util.Optional;
 @RequestMapping("/api/search")
 public class SearchController {
 
+    private static Logger log = LoggerFactory.getLogger(SearchController.class);
+
     @Autowired
     private SearchService search;
 
     @RequestMapping(value = {""} , method = RequestMethod.GET)
     public List<Document> getDocument(@RequestParam(value = "lang") Optional<String> lang) {
+
+        log.info("{ method: getDocument, params: [ {} ] }",lang.orElse("default"));
 
         if(lang.isPresent())
             return search.get(lang.get());
@@ -27,8 +32,9 @@ public class SearchController {
 
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public List<Document> getDocument(@PathVariable("id") String id, @RequestParam(value = "lang") Optional<String> lang ) {
+    public List<Document> getDocumentById(@PathVariable("id") String id, @RequestParam(value = "lang") Optional<String> lang ) {
 
+        log.info("{ method: getDocumentById, params: [{} , {}] }",id,lang.orElse("default"));
         if(lang.isPresent())
             return search.getById(id, lang.get());
         else
